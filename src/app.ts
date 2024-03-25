db09860219b34841cb339be79e00713ebbc1101c
raihanuldev <rihanthedev@gmail.com>
2024-03-25 23:05:57 +0600

    added:

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 const app: Application = express()
-const port = 3000
 
 // useing cors
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-export default app;
+export default app
